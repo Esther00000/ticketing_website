2629ab8946a3eefad733ac0d630ad722e0a5f739
Esther <fuest8456258@gmail.com>
2023-08-28 23:41:59 +0800

    Updates

---
+++ assets/index-2ee3f428.js
@@ -0,0 +1,1 @@
+import"./bootstrap.min-d42e2b77.js";import e from"https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs";new e(".mySwiper",{allowTouchMove:!1,slidesPerView:"auto",speed:5e3,loop:!0,autoplay:{delay:0,disableOnInteraction:!1}});
